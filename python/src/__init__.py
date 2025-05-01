5805b42b242ec460b52616ebcba0249278a14466
Willem Veelenturf <willem.veelenturf@ing.com>
2025-05-01 13:14:06 +0200

    Python

--- python/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file exposes the modules in the src directory to make them importable
-from . import api
-from . import client
-from . import context
-from . import signing
-from . import wirespec
