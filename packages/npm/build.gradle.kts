e95cbf95f59275429816089d4e44cba6fac8225d
Willem Veelenturf <willem.veelenturf@ing.com>
2025-06-04 17:15:36 +0200

    Refactor build system to use Gradle for Wirespec task
    Replaced npm-based Wirespec generation with a Gradle-based workflow. Updated `build.yml` to configure Java for Gradle and removed unused dependencies and scripts from `package.json`. This improves maintainability and aligns the project with Gradle’s capabilities.

---
+++ packages/npm/build.gradle.kts
@@ -0,0 +1,35 @@
+import community.flock.wirespec.compiler.core.emit.TypeScriptEmitter
+import community.flock.wirespec.compiler.core.emit.common.EmitShared
+import community.flock.wirespec.plugin.Format
+import community.flock.wirespec.plugin.gradle.ConvertWirespecTask
+
+plugins {
+    alias(libs.plugins.wirespec)
+}
+
+group = "community.flock.wirespec.example.gradle"
+version = libs.versions.wirespec.get()
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.wirespec.compiler)
+    }
+}
+
+tasks.register<ConvertWirespecTask>("wirespec") {
+    output = layout.projectDirectory.dir("src")
+    input = layout.projectDirectory.file("../../openapi.json")
+    packageName = "api"
+    emitterClass = SdkTypeScriptEmitter::class.java
+    format = Format.OpenAPIV3
+    strict = true
+    shared = true
+}
+
+class SdkTypeScriptEmitter(emitShared: EmitShared) : TypeScriptEmitter(emitShared) {
+}
