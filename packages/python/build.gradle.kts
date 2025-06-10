d8a9edee516eb3274de72f4db4f52ec9d6f35816
Willem Veelenturf <willem.veelenturf@ing.com>
2025-06-10 19:55:54 +0200

    Fix group name

--- packages/python/build.gradle.kts
+++ packages/python/build.gradle.kts
@@ -32,7 +32,7 @@ tasks.register<ConvertWirespecTask>("wirespec") {
     format = Format.OpenAPIV3
     strict = true
     shared = true
-    preProcessor = { it -> it }
+    preProcessor = OpenApiPreProcessor
 }
 
 class SdkPythonEmitter(val packageName: PackageName, emitShared: EmitShared): PythonEmitter(packageName, emitShared) {
