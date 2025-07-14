62252c27f76ba38ae26ecb4ec79bab388c584d6b
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-14 17:06:19 +0200

    Fix python sdk

--- packages/python/build.gradle.kts
+++ packages/python/build.gradle.kts
@@ -41,17 +41,33 @@ class SdkPythonEmitter(val packageName: PackageName, emitShared: EmitShared): Py
             .let { it + Emitted("${packageName.toDir()}/sdk", """
                 |from . import endpoint
                 |
-                |class Sdk(
-                |${module.statements.filterIsInstance<Endpoint>().joinToString(",\n") {endpoint ->  "endpoint.${emit(endpoint.identifier)}.Handler" }.spacer(1)}
-                |):
+                |from typing import List, Optional
+                |
+                |class Sdk():
                 |
                 |  def __init__(self, handler, serialization):
                 |    self.handler = handler
                 |    self.serialization = serialization
-                | 
-                |${module.statements.filterIsInstance<Endpoint>().joinToString("\n") {endpoint ->  "def ${emit(endpoint.identifier)}(self, req): return self.handler(self.serialization, endpoint.${emit(endpoint.identifier)}, req)" }.spacer(1)}
+                |
+                |${module.emitEndpointRequest("\n") { (endpoint, request) -> emitFunction(endpoint, request) }.spacer(1)}
+                |
             """.trimMargin()) }
     }
+
+    fun Endpoint.Request.emitSdkInterface(endpoint: Endpoint) =
+        this.paramList(endpoint).joinToString(", ") { "${emit(it.identifier)}: ${it.reference.emit()}" }
+
+    fun emitFunction(endpoint: Endpoint, request: Endpoint.Request) = """
+        |def ${emit(endpoint.identifier).firstToLower()}(self, ${request.emitSdkInterface(endpoint)}):
+        |   req = endpoint.${emit(endpoint.identifier)}.Request${request.paramList(endpoint).takeIf { it.size > 0 }?.joinToString(", ", "(", ")") { emit(it.identifier) }.orEmpty()}
+        |   return self.handler(self.serialization, endpoint.${emit(endpoint.identifier)}, req)
+    """.trimMargin()
+
+    fun Module.emitEndpointRequest(separator: CharSequence, emit: (Pair<Endpoint, Endpoint.Request>) -> String) =
+        statements
+            .filterIsInstance<Endpoint>()
+            .flatMap { endpoint -> endpoint.requests.map { request -> Pair(endpoint, request) } }
+            .joinToString(separator) { endpointRequest -> emit(endpointRequest) }
 }
 
 tasks.register<Exec>("mypy") {
--- packages/python/src/sdk_test.py
+++ packages/python/src/sdk_test.py
@@ -1,33 +1,32 @@
 import unittest
 
 from api.endpoint import READ_User, List_all_MonetaryAccountBank_for_User
-
+from api.sdk import Sdk
 from context import Context
 from wirespec import Serialization, handler
-from api.sdk import Sdk
 
 USER_API_KEY = "sandbox_83f4f88a10706750ec2fdcbc1ce97b582a986f2846d33dcaaa974d95"
-service_name='PeterScript'
+service_name = 'PeterScript'
 
 serialization = Serialization()
 sdk = Sdk(handler, serialization)
 context = Context(USER_API_KEY, service_name)
 
+
 class Testing(unittest.TestCase):
 
     def test_read_user_endpoint_test(self):
-        req = READ_User.Request(
-            itemId = context.user_id,
-            CacheControl = None,
-            UserAgent = context.service_name,
-            XBunqLanguage = None,
-            XBunqRegion = None,
-            XBunqClientRequestId = None,
-            XBunqGeolocation = None,
-            XBunqClientAuthentication = context.session_token,
-        )
 
-        res = sdk.READ_User(req)
+        res = sdk.READ_User(
+            itemId=context.user_id,
+            CacheControl=None,
+            UserAgent=context.service_name,
+            XBunqLanguage=None,
+            XBunqRegion=None,
+            XBunqClientRequestId=None,
+            XBunqGeolocation=None,
+            XBunqClientAuthentication=context.session_token,
+        )
 
         match res:
             case READ_User.Response200(body):
@@ -36,25 +35,25 @@ class Testing(unittest.TestCase):
 
     def test_list_all_monetary_account_bank_for_User(self):
 
-        req = List_all_MonetaryAccountBank_for_User.Request(
-            userID = context.user_id,
-            CacheControl = None,
-            UserAgent = context.service_name,
-            XBunqLanguage = None,
-            XBunqRegion = None,
-            XBunqClientRequestId = None,
-            XBunqGeolocation = None,
-            XBunqClientAuthentication = context.session_token,
+        res = sdk.List_all_MonetaryAccountBank_for_User(
+            userID=context.user_id,
+            CacheControl=None,
+            UserAgent=context.service_name,
+            XBunqLanguage=None,
+            XBunqRegion=None,
+            XBunqClientRequestId=None,
+            XBunqGeolocation=None,
+            XBunqClientAuthentication=context.session_token,
         )
-
-        res = sdk.List_all_MonetaryAccountBank_for_User(req)
 
         match res:
             case List_all_MonetaryAccountBank_for_User.Response200(body):
                 self.assertEqual(body[0].display_name, "D. Byrne")
 
+
 def main():
     unittest.main()
+
 
 if __name__ == "__main__":
     main()
