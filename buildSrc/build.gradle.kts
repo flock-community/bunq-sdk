df7f77ed3f0633c47b98e45a7ccc1a4524b75f3c
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-15 16:48:16 +0200

    Bump Wirespec

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("community.flock.wirespec.plugin.gradle") version "0.15.2"
+    id("community.flock.wirespec.plugin.gradle") version "0.16.3"
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
 }
@@ -11,6 +11,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("community.flock.wirespec.compiler:core-jvm:0.15.2")
+    implementation("community.flock.wirespec.compiler:core-jvm:0.16.3")
     implementation("community.flock.kotlinx.openapi.bindings:kotlin-openapi-bindings-jvm:0.0.25")
 }
