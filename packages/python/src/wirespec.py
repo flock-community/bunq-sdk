260fdcf5fdeb6efa7f26b3c2ab184769fdf763ef
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-15 16:55:48 +0200

    Fix mypy

--- packages/python/src/wirespec.py
+++ packages/python/src/wirespec.py
@@ -9,7 +9,12 @@ def handler(signing: Signing, context: Context):
     token = context.session_token
 
 
+
     def handle(endpoint:Wirespec.Endpoint, req: Wirespec.Request) -> Wirespec.Response:
+
+        if token is None:
+            raise Exception("Token is None")
+
         raw_req: Wirespec.RawRequest = endpoint.Convert.to_raw_request(serialization, req)
         raw_req.headers["X-Bunq-Client-Authentication"] = [token]
         raw_res = send(signing, raw_req)
